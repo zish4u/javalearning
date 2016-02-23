8a4d6c71a757c2b4c50f8541bf69e76a64b6a477
zish <zish4u@gmail.com>
2016-02-23 10:55:47 +0530

    String Pattern

---
+++ Pattern.java
@@ -0,0 +1,14 @@
+/*Print pattern like b-c-a-b
+                     a-b-
+					 - */
+					 
+public class Pattern{
+	public static void main(String[] args){
+		String a="a-b";
+		String b="b-c";
+		String c="-";
+		System.out.println(b+c+a);
+		System.out.println(a+c);
+		System.out.println(c);
+	}
+}					 
