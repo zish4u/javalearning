85a9035b91c1adf1d7ca54d45332cf1ebc39ce23
zish <zish4u@gmail.com>
2016-03-16 20:07:02 +0530

    Print Patterns

--- PrintPatterns.java
+++ PrintPatterns.java
@@ -1,5 +1,5 @@
 public class PrintPatterns{
-	
+	//code to print square box pattern.
 	public void pattern1(){
 		int row=4,col=4;
 		for(int i=1;i<=row;i++){
@@ -9,7 +9,7 @@ public class PrintPatterns{
 			System.out.println();
 		}
 	}
-	
+	//code to print number pattern.
 	public void pattern2(){
 		int row=4,i=1;
 		while(i<=row){
@@ -18,11 +18,11 @@ public class PrintPatterns{
 				System.out.print(j);
 				j++;
 			}
-			System.out.println("");
+			System.out.println();
 			i++;
 		}
 	}
-	
+	//code to print number pattern.
 	public void pattern3(){
 		int row=4;int i=1; 
 		while(i<=row){
@@ -36,7 +36,7 @@ public class PrintPatterns{
 	    }
 		
 	}
-	
+	////code to print pattern.
 	public void pattern4(){
 		int row=4,col=4;
 		for(int i=1;i<=row;i++){
@@ -50,14 +50,13 @@ public class PrintPatterns{
 			
 		}
 	}
-	
+	//code to print empty square box pattern.
 	public void pattern5(){
 		int row=5,col=5;
 		for(int i=1;i<=row;i++){
 			for(int j=1;j<=col;j++){
 				if((i==1||i==5)||(j==1||j==5)){
 				    System.out.print("#");
-					//System.out.println("");
 			    }
 				else{
 					System.out.print(" ");
@@ -66,9 +65,9 @@ public class PrintPatterns{
 			System.out.println("");
 		}
 	}
-	
+	//code to print reverse z-shape pattern.
 	public void pattern6(){
-		int row=5,col=5;
+		//int row=5,col=5;
 		for(int i=1;i<=5;i++){
 			for(int j=1;j<=5;j++){
 				if((i==1||i==5)||(i==j))
@@ -80,17 +79,57 @@ public class PrintPatterns{
 				
 		}
 	}
-	
+	//code to print z-shape pattern.
 	public void pattern7(){
 		int row=5,col=5;
-		for(int i=1;i<=5;i++){
-			for(int j=1;j<=5;j++){
-				if((i==1||i==5)||(j%2==0||i==j))
+		for(int i=1;i<=row;i++){
+			for(int j=1;j<=col;j++){
+				if((i==1||i==5||(i==3&&j==3))||(j==2*i)||(i!=2&&i==2*j))
 					System.out.print("#");
 				else
 					System.out.print(" ");
 			}
 			System.out.println("");
+		}
+	}
+	//code to print reverse isosceles triangle pattern.
+	public void pattern8(){
+		int row=5,col=9;
+		for(int i=1;i<=row;i++){
+			for(int j=1;j<=col;j++){
+			if((i==j)||(i==1&&j%2!=0)||(i==2&&j==4*i)||(i==3&&j==i+4)||(i==4&&j==i+2)){
+					System.out.print("*");
+				}
+				else 
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+	//code to print isosceles triangle pattern.
+	public void pattern9(){
+		int row=5,col=9;
+		for(int i=1;i<=row;i++){
+			for(int j=1;j<=col;j++){
+				if((i==5&&j%2!=0)||j==i+4||(i==3&&j==3)||(i==2&&j==2*i)||(i==4&&j==i/2))
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
+		}
+	}
+	
+	public void pattern10(){
+		int row=5,col=5;
+		for(int i=1;i<=row;i++){
+			for(int j=1;j<=col;j++){
+				if((j%2==0&&i%2==0)||j==i+2||(i==1&&j==3)||(i==5&&j==3)||(i==3&&j==1))
+					System.out.print("*");
+				else
+					System.out.print(" ");
+			}
+			System.out.println();
 		}
 	}
 	
@@ -103,5 +142,8 @@ public class PrintPatterns{
 		obj.pattern5();
 		obj.pattern6();
 		obj.pattern7();
+		obj.pattern8();
+		obj.pattern9();
+		obj.pattern10();
 	}
 }
